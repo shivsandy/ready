bfcce0e8b94a63ed598f43cc61858400b64f71ac
shiva8yte <71714299+shivsandy@users.noreply.github.com>
2025-04-06 23:44:05 -0700

    Update uninstaller.bat

--- uninstaller.bat
+++ uninstaller.bat
@@ -2,6 +2,7 @@
 :: Define the folder path and process name
 set "folderPath=%LocalAppData%\FixResolution"
 set "processName=resolution.exe"
+set "shortcutName=resolution"
 
 :: Stop the resolution.exe process if it's running
 echo Attempting to kill the %processName% process...
@@ -23,6 +24,21 @@ if exist "%folderPath%" (
     echo Failed to delete the FixResolution folder.
 ) else (
     echo FixResolution folder deleted successfully.
+)
+
+:: Delete the shortcut from the Startup folder
+set "startupFolder=%AppData%\Microsoft\Windows\Start Menu\Programs\Startup"
+echo Deleting the shortcut from Startup folder...
+del /f /q "%startupFolder%\%shortcutName%.lnk" >nul 2>&1
+del /f /q "%startupFolder%\%shortcutName%.exe" >nul 2>&1
+
+:: Confirm deletion
+if exist "%startupFolder%\%shortcutName%.lnk" (
+    echo Failed to delete %shortcutName%.lnk from Startup folder.
+) else if exist "%startupFolder%\%shortcutName%.exe" (
+    echo Failed to delete %shortcutName%.exe from Startup folder.
+) else (
+    echo Startup shortcut deleted successfully.
 )
 
 :: Wait 2 seconds before closing
